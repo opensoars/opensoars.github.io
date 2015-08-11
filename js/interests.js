3684681caeb3a74b53ceb0edc9c60d5b30a7a320
unknown <sam_1700@hotmail.com>
2015-08-11 14:17:59 +0200

    Minor code quality upgrade

--- js/interests.js
+++ js/interests.js
@@ -31,32 +31,37 @@ close_btn.addEventListener('click', function () {
 });
 */
 
-function domListToArray(dom_list) {
-  var arr = [];
 
-  for (var i = 0; i < dom_list.length; i++) {
-    arr.push(dom_list[i]);
+(function () {
+  /**
+   * Turns DOM elements lists into actual arrays.
+   * @param {object} dom_list - DOM elements list object return by DOM query
+   * @return {array} arr - Actual DOM elements array
+   */
+  function domListToArray(dom_list) {
+    var arr = [];
+
+    for (var i = 0; i < dom_list.length; i++) {
+      arr.push(dom_list[i]);
+    }
+
+    return arr;
   }
 
-  return arr;
-}
+  var interests_list = document.getElementById('interests_list'),
+      interests_list_left = document.getElementById('interests_list_left'),
+      interests_list_right = document.getElementById('interests_list_right');
 
-var interests_list = document.getElementById('interests_list'),
-    interests_list_left = document.getElementById('interests_list_left'),
-    interests_list_right = document.getElementById('interests_list_right');
+  var interests_list_children = domListToArray(interests_list.children);
 
+  var left_count = Math.ceil(interests_list_children.length / 2);
 
-var interests_list_children = domListToArray(interests_list.children);
-
-
-var left_count = Math.ceil(interests_list_children.length / 2);
-
-
-interests_list_children.forEach(function (child, i) {
-  if (i < left_count) {
-    interests_list_left.appendChild(child.cloneNode(true));
-  }
-  else {
-    interests_list_right.appendChild(child.cloneNode(true));
-  }
-});
+  interests_list_children.forEach(function (child, i) {
+    if (i < left_count) {
+      interests_list_left.appendChild(child.cloneNode(true));
+    }
+    else {
+      interests_list_right.appendChild(child.cloneNode(true));
+    }
+  });
+}());
